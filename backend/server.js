bfe7c11f4cf660938c86993eaf528e6e5feae981
Kadi Karim <kadikarimpr@gmail.com>
2021-03-16 16:32:14 +0100

    responsive design

--- backend/server.js
+++ backend/server.js
@@ -39,6 +39,6 @@ if (process.env.NODE_ENV === 'production') {
 app.use(notFound)
 app.use(errorHandler)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => `The servers is running on port ${PORT}`)
