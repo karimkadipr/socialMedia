bc076c42d4783e2bd774c24ebde49ceeb209b646
Kadi Karim <kadikarimpr@gmail.com>
2021-03-18 11:12:50 +0100

    adding verified accounts

--- backend/user/userModel.js
+++ backend/user/userModel.js
@@ -21,6 +21,11 @@ const userSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
   },
   {
     timestamps: true,
